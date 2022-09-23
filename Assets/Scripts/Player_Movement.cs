a610a7c1f1e7a3c1bf12da8409ca192a9da01bd2
Adrien <a.poignet12@orange.fr>
2022-09-23 17:09:53 +0200

    début animation player

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,67 +6,81 @@ public class Player_Movement : MonoBehaviour
 {
 
     Vector3 position;
+
     private float jumpState;
     private bool onground = false;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         jumpState = 0;
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
         //zqsd
         position = transform.position;
 
         if (Input.GetKey("z"))
         {
+            animator.SetBool("IsWalking", true);
+
             position.z += 0.08f;
             transform.position = position;
         }
         if (Input.GetKey("q"))
         {
+            animator.SetBool("IsWalking", true);
+
             position.x -= 0.08f;
             transform.position = position;
         }
         if (Input.GetKey("s"))
         {
+            animator.SetBool("IsWalking", true);
+
             position.z -= 0.08f;
             transform.position = position;
         }
         if (Input.GetKey("d"))
         {
+            animator.SetBool("IsWalking", true);
+
             position.x += 0.08f;
             transform.position = position;
         }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
 
         //jump
-        if(Input.GetKeyDown("space") && jumpState == 0 && onground == true)
+        if (Input.GetKeyDown("space") && jumpState == 0 && onground == true)
         {
             jumpState = 1;
         }
-        
-        if(jumpState > 0)
+
+        if (jumpState > 0)
         {
             position.y += 0.11f;
             transform.position = position;
 
             jumpState -= 0.07f;
-            if(jumpState < 0)
+            if (jumpState < 0)
             {
                 jumpState = 0;
             }
         }
 
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        print("aled");
-
         if (other.tag == "Ground")
         {
             onground = true;
